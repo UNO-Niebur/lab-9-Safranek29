31393ca80e1b3690d95c12cd8441ed336ccd372c
Safranek29 <166367939+Safranek29@users.noreply.github.com>
2026-04-01 21:42:19 +0000

    finished Lab 9

--- ImageFilters.py
+++ ImageFilters.py
@@ -1,16 +1,21 @@
 # Lab 9 – Image Processing
-# Name:
-# Date:
-# Assignment:
+# Name: Louis Safranek
+# Date: 04-01-2026
+# Assignment: Lab 9
 
 from PIL import Image
 
 
 def swapGreenBlue(img):
     """Swap the green and blue values for every pixel in the image."""
-    
     pixels = img.load()
     width, height = img.size
+    for x in range(width):
+        for y in range(height):
+            red, green, blue, alpha = pixels[x,y]
+            pixels[x,y] = (red, blue, green, alpha)
+    
+
 
     # TODO: Loop through every pixel and swap green and blue values
 
@@ -19,9 +24,18 @@ def swapGreenBlue(img):
 
 def darken(img, amount):
     """Darken the image by reducing RGB values by the given amount."""
-    
     pixels = img.load()
     width, height = img.size
+
+    for x in range(width):
+        for y in range(height):
+            red, green, blue, alpha = pixels[x,y]
+            red = max(red-amount, 0)
+            green = max(green-amount, 0)
+            blue = max(blue-amount, 0)
+            pixels[x,y] = (red, green, blue, alpha)
+    
+
 
     # TODO: Loop through every pixel and reduce RGB values by amount
     # Make sure values do not go below 0
@@ -37,9 +51,9 @@ def bwFilter(img):
 
     for x in range(width):
         for y in range(height):
-            red, green, blue = pixels[x, y]
+            red, green, blue, alpha = pixels[x, y]
             avg = (red + green + blue) // 3
-            pixels[x, y] = (avg, avg, avg)
+            pixels[x, y] = (avg, avg, avg, alpha)
 
     img.save("bwImg.png")
 
@@ -49,11 +63,11 @@ def main():
     myImg = Image.open("durango.png")
 
     # Example (already completed)
-    # bwFilter(myImg)
+    #bwFilter(myImg)
 
     # Uncomment each function as you complete it
-    # swapGreenBlue(myImg)
-    # darken(myImg, 20)
+    #swapGreenBlue(myImg)
+    darken(myImg, 100)
 
 
 if __name__ == "__main__":
